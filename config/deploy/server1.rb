c6324497a253629014977236938275ad0d4bad45
Robikos <r.a.kostrzewski@gmail.com>
2018-05-23 00:52:45 +0200

    Fix executing docker compose

--- config/deploy/server1.rb
+++ config/deploy/server1.rb
@@ -24,7 +24,7 @@ namespace :deploy do
     on roles(:app) do
       within release_path do
         ## deploy stack
-        execute :"docker-compose", "up -d --force-recreate -f docker-compose-server1.yml"
+        execute :"docker-compose", "-d --force-recreate -f docker-compose-server1.yml up"
       end
     end
   end
